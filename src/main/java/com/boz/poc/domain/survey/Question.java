64dd6aa6016cafbaff114e653a255955f61f4de2
jboz <julienboz@gmail.com>
2012-09-11 13:10:49 +0200

    survey v2 - dynamic

--- src/main/java/com/boz/poc/domain/survey/Question.java
+++ src/main/java/com/boz/poc/domain/survey/Question.java
@@ -1,34 +1,108 @@
 package com.boz.poc.domain.survey;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Question implements Serializable {
+public class Question<T> implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private String code;
-	private boolean selected = false;
-	private Map<String, String> responses = new HashMap<>();
+	private boolean mandatory = false;
+	private boolean multiResponse = true;
+	private List<Question<?>> subQuestions = new ArrayList<>();
+	private Question<?> parent;
+	private FormType formType;
+
+	private Class<T> responseType;
+	private List<Response<T>> responses = new ArrayList<>();
 
 	public String getCode() {
 		return code;
+	}
+
+	public String getFinalCode() {
+		return (parent != null ? parent.getFinalCode() + "." : "") + code;
 	}
 
 	public void setCode(final String code) {
 		this.code = code;
 	}
 
-	public Map<String, String> getResponses() {
+	public int getNbSubResponses() {
+		int nb = 0;
+		for (final Question<?> question : subQuestions) {
+			nb = Math.max(nb, question.getResponses().size());
+		}
+		// create one if empty
+		return (nb < 1 ? 1 : nb) - 1;
+	}
+
+	public List<Response<T>> getResponses() {
 		return responses;
 	}
 
-	public boolean isSelected() {
-		return selected;
+	public Response<T> getFirstResponse() {
+		return getResponse(0);
 	}
 
-	public void setSelected(final boolean selected) {
-		this.selected = selected;
+	public Response<T> getResponse(final int index) {
+		while (responses.size() <= index) {
+			addResponse();
+		}
+		return responses.get(responses.size() - 1);
 	}
 
+	private Response<T> addResponse() {
+		final Response<T> response = new Response<>();
+		response.setQuestion(this);
+		responses.add(response);
+
+		return response;
+	}
+
+	public FormType getFormType() {
+		return formType;
+	}
+
+	public void setFormType(final FormType formType) {
+		this.formType = formType;
+	}
+
+	public boolean isMandatory() {
+		return mandatory;
+	}
+
+	public void setMandatory(final boolean mandatory) {
+		this.mandatory = mandatory;
+	}
+
+	public boolean isMultiResponse() {
+		return multiResponse;
+	}
+
+	public void setMultiResponse(final boolean multiResponse) {
+		this.multiResponse = multiResponse;
+	}
+
+	public List<Question<?>> getSubQuestions() {
+		return subQuestions;
+	}
+
+	public void addSubQuestions(final Question<?> question) {
+		subQuestions.add(question);
+		question.parent = this;
+	}
+
+	public Question<?> getParent() {
+		return parent;
+	}
+
+	public Class<?> getResponseType() {
+		return responseType;
+	}
+
+	public void setResponseType(final Class<T> responseType) {
+		this.responseType = responseType;
+	}
 }
--- src/main/java/com/boz/poc/presentation/survey/SurveyBean.java
+++ src/main/java/com/boz/poc/presentation/survey/SurveyBean.java
@@ -1,5 +1,6 @@
 package com.boz.poc.presentation.survey;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,28 +8,96 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import com.boz.poc.domain.survey.FormType;
 import com.boz.poc.domain.survey.Question;
 
 @ManagedBean
 @SessionScoped
-public class SurveyBean {
+public class SurveyBean implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-	public List<Question> questions = new ArrayList<>();
+	public List<Question<?>> questions = new ArrayList<>();
+
+	public List<Question<?>> getQuestions() {
+		return questions;
+	}
 
 	@PostConstruct
 	public void init() {
-		questions.add(createQuestion("poidsTaille"));
-		questions.add(createQuestion("medecinTraitant"));
+		questions.add(createTaillePoids());
+		questions.add(createFumezVous());
+		questions.add(createMedecinTraitant());
 	}
 
-	private static Question createQuestion(final String code) {
-		final Question question = new Question();
-		question.setCode(code);
+	private Question<String> createMedecinTraitant() {
+		final Question<String> question =  createQuestion("medecinTraitant", false, FormType.COLLAPSIBLE, true);
+		question.addSubQuestions(createQuestion("nom", FormType.TEXT_FIELD));
+		question.addSubQuestions(createQuestion("prenom", FormType.TEXT_FIELD));
+		question.addSubQuestions(createQuestion("rue", FormType.TEXT_FIELD));
+		question.addSubQuestions(createQuestion("npa", FormType.TEXT_FIELD));
+		question.addSubQuestions(createQuestion("ville", FormType.TEXT_FIELD));
 
 		return question;
 	}
 
-	public List<Question> getQuestions() {
-		return questions;
+	private Question<String> createFumezVous() {
+		final Question<String> question = createQuestion("fumezVous", false, FormType.COLLAPSIBLE, true);
+		question.addSubQuestions(createQuestion("quoi", FormType.COMBO, FumeQuoi.class));
+		question.addSubQuestions(createQuestion("frequence", FormType.COMBO, FumeFrequence.class));
+
+		find(0, question).getResponse(0).setValue(FumeQuoi.CIGARETTE);
+		find(1, question).getResponse(0).setValue(FumeFrequence.MAX20);
+		find(0, question).getResponse(1).setValue(FumeQuoi.JOINT);
+		find(1, question).getResponse(1).setValue(FumeFrequence.MAX5);
+
+		return question;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T> Question<T> find(final int index, final Question<?> question) {
+		return (Question<T>) question.getSubQuestions().get(index);
+	}
+
+	private Question<String> createTaillePoids() {
+		final Question<String> question = createQuestion("taillePoids", FormType.COLLAPSIBLE);
+		question.addSubQuestions(createQuestion("taille", FormType.TEXT_FIELD));
+		question.addSubQuestions(createQuestion("poids", FormType.TEXT_FIELD));
+
+		return question;
+	}
+
+	private Question<String> createQuestion(final String code, final FormType type) {
+		return createQuestion(code, true, type, false);
+	}
+
+	private <T> Question<T> createQuestion(final String code, final FormType type, final Class<T> reponseType) {
+		return createQuestion(code, true, type, false, reponseType);
+	}
+
+	private Question<String> createQuestion(final String code, final boolean mandatory, final FormType type,
+			final boolean multiResponse) {
+		return createQuestion(code, mandatory, type, multiResponse, String.class);
+	}
+
+	private <T> Question<T> createQuestion(final String code, final boolean mandatory, final FormType type,
+			final boolean multiResponse, final Class<T> reponseType) {
+		final Question<T> question = new Question<>();
+		question.setCode(code);
+		question.setFormType(type);
+		question.setMultiResponse(multiResponse);
+		question.setResponseType(reponseType);
+		return question;
+	}
+
+	public static enum FumeQuoi {
+		CIGARETTE, JOINT, AUTRE;
+	}
+
+	public static enum FumeFrequence {
+		MAX5, MAX10, MAX20, NO_LIMIT;
+	}
+
+	public <T> Object[] values(final Class<T> type) {
+		return type.getEnumConstants();
 	}
 }
